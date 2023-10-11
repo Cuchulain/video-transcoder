e56102860d5bd7c7641757b4691c417355fba4b9
Jan Čejka <posta@jancejka.cz>
2023-10-11 14:39:47 +0200

    add first test

---
+++ test_recoder.py
@@ -0,0 +1,14 @@
+import recode_video
+
+
+def test_recode_video():
+    metadata = {
+        'tags': {
+            'title': 'test',
+        }
+    }
+
+    assert recode_video.get_language_title('cze', metadata) == 'Czech'
+    assert recode_video.get_language_title('slo', metadata) == 'Slovak'
+    assert recode_video.get_language_title('eng', metadata) == 'English'
+    assert recode_video.get_language_title('ger', metadata) == 'test'
